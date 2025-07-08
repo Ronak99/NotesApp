5ef21a5270a377882924bab1880ca4ffe68d5f80
Ronak99 <punase.ronak99@gmail.com>
2025-07-08 23:37:35 +0530

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Ronak Punase on 08/07/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
