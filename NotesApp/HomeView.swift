4ad00bdfbeeeae0a28e822b934ba280d7a952b40
Ronak99 <punase.ronak99@gmail.com>
2025-07-14 00:25:16 +0530

    Added custom swiftdata preview

---
+++ NotesApp/HomeView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeView.swift
+//  NotesApp
+//
+//  Created by Ronak Punase on 13/07/25.
+//
+
+import Foundation
---
+++ NotesApp/Preview/Preview.swift
@@ -0,0 +1,37 @@
+//
+//  Preview.swift
+//  NotesApp
+//
+//  Created by Ronak Punase on 13/07/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct Preview {
+    let modelContainer : ModelContainer
+    
+    init() {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        do {
+            modelContainer = try ModelContainer(for: Note.self, configurations: config)
+        } catch {
+            fatalError("Could not initiate the model container")
+        }
+    }
+    
+    func addSamples(_ samples: [Note]) {
+        Task {
+            @MainActor in samples.forEach {
+                note in modelContainer.mainContext.insert(note)
+            }
+        }
+    }
+}
+ 
+#Preview {
+    let preview = Preview()
+    let _ = preview.addSamples(Note.sampleItems)
+    ContentView()
+        .modelContainer(preview.modelContainer)
+}
