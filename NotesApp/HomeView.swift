6a21cdf66c8f414bed8990261ee03bb81d6ae6ac
Ronak99 <punase.ronak99@gmail.com>
2025-07-14 00:25:37 +0530

    create homeview

--- NotesApp/HomeView.swift
+++ NotesApp/HomeView.swift
@@ -5,4 +5,29 @@
 //  Created by Ronak Punase on 13/07/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct HomeView: View {
+    let notes: [Note]
+    
+    init(notes: [Note]) {
+        self.notes = notes
+    }
+    
+    var body: some View {
+        if notes.isEmpty {
+            VStack (spacing: 8,) {
+                Text("Welcome").font(.largeTitle)
+                Text("Create your first note by tapping the + icon").font(.subheadline)
+            }
+        } else {
+            List(notes) { note in
+                NavigationLink {
+                    NoteDetailView(note: note)
+                } label: {
+                    NoteListItem(note: note)
+                }
+            }
+        }
+    }
+}
